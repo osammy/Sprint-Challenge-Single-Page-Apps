8b17f66aa3fcc03a6a81e4d53aa4ae63e9a7906f
samuel <osamaimafidon@gmail.com>
2019-09-20 04:53:11 +0100

    Home, Character, Location and Episodes Tab complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
-import TabNav from "./components/TabNav.js";
+import TabNav from "./components/views/Tabs/TabNav.js";
 import Header from "./components/Header.js";
+import {getPanes} from "./components/views/Tabs/TabComponents.js";
+import { getCharacters, getLocations, getEpisodes } from "./data";
 
-
+const characters = getCharacters();
+const locations = getLocations();
+const episodes = getEpisodes();
 export default function App() {
   return (
     <main>
       <Header />
-      {/* <TabNav /> */}
+       <TabNav panes={  getPanes({characters,locations, episodes})()  } /> 
     </main>
   );
 }
